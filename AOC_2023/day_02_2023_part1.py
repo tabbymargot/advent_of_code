c54345d1ce923d2e412ad5a4d3d774b8687c48a0
Caroline Wood <carolinewood72@gmail.com>
2024-10-24 14:31:17 +0100

    Added comment to test new branch

--- AOC_2023/day_02_2023_part1.py
+++ AOC_2023/day_02_2023_part1.py
@@ -1,3 +1,5 @@
+# TESTING THE NEW BRANCH
+
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green - possible
 # Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue -  possible
 # Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red - impossible
