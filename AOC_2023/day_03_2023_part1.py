bf13c987d6f96cfa92038427de553cde8b0f13d9
Caroline Wood <carolinewood72@gmail.com>
2024-10-25 18:30:09 +0100

    more changes

--- AOC_2023/day_03_2023_part1.py
+++ AOC_2023/day_03_2023_part1.py
@@ -21,20 +21,85 @@ def convert_line_input_to_list_of_chars(string1, string2, string3):
         for char in each_string:
             my_string = f'{char}'
             nested_list[idx].append(my_string)
-            print(idx)
-            print(my_string)
 
-        print(nested_list)
+    print(nested_list)
 
-string1 = '...'
-string2 = '***'
-string3 = '$$$'
+string1 = '.......'
+string2 = '*******'
+string3 = '$$$$$$$'
 
-convert_line_input_to_list_of_chars(string1, string2, string3)
+# convert_line_input_to_list_of_chars(string1, string2, string3)
 
-# xs = [8, 23, 45]
+# *******************************
 
-# for idx, x in enumerate(xs):
-#     print(idx, x)
+# GET THE NEXT 3 STRINGS
+
+def get_newline_occurence_for_substr_start(input_string):
+    counter = 1
+    total_newline_chars = input_string.count("\n")
+
+    while counter <= total_newline_chars:
+        newline_occurence_needed_for_start = counter
+        break
+        # counter += 1
+    return newline_occurence_needed_for_start
+
+def get_newline_occurence_for_substr_end(input_string):
+    counter = 4
+    total_newline_chars = input_string.count("\n")
+
+    while counter <= total_newline_chars:
+        newline_occurence_needed_for_end = counter
+        break
+        # counter += 1
+    return newline_occurence_needed_for_end
+
+
+def find_index_for_substr_start(input_string, newline_char, newline_occurence_needed_for_start):
+    count = 0
+    for i in range(len(input_string)):
+        if input_string[i:i+len(newline_char)] == newline_char:
+            count += 1
+            if count == newline_occurence_needed_for_start:
+                return i
+    return -1
+
+def find_index_for_substr_end(input_string, newline_char, newline_occurence_needed_for_end):
+    count = 0
+    for i in range(len(input_string)):
+        if input_string[i:i+len(newline_char)] == newline_char:
+            count += 1
+            if count == newline_occurence_needed_for_end:
+                return i
+    return -1
+
+
+input_string = '''
+467..114..
+...*......
+..35..633.
+......#...
+'''
+newline_char = "\n"
+
+newline_occurence_needed_for_start = get_newline_occurence_for_substr_start(input_string)
+index_for_substr_start = find_index_for_substr_start(input_string, newline_char, newline_occurence_needed_for_start)
+
+newline_occurence_needed_for_end = get_newline_occurence_for_substr_end(input_string)
+
+index_for_substr_end = find_index_for_substr_end(input_string, newline_char, newline_occurence_needed_for_end)
+
+print(index_for_substr_start)
+print(index_for_substr_end)
+
+
+
+# input_string = '''
+# 467..114..
+# ...*......
+# ..35..633.
+# ......#...
+# '''
+# print(set_starting_point(input_string))
 
 
