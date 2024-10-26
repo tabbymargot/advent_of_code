5035eb8acc18ab5a900b4615783a5364a51adb01
Caroline Wood <carolinewood72@gmail.com>
2024-10-26 16:33:17 +0100

    added functions to split the substring and convert the split strings to sublists

--- AOC_2023/day_03_2023_part1.py
+++ AOC_2023/day_03_2023_part1.py
@@ -15,14 +15,14 @@ def convert_line_input_to_list_of_chars(string1, string2, string3):
 
     list_of_strings = [string1, string2, string3]
 
-    nested_list = [[],[],[]]
+    list_with_sublists = [[],[],[]]
 
     for idx, each_string in enumerate(list_of_strings):
         for char in each_string:
             my_string = f'{char}'
-            nested_list[idx].append(my_string)
+            list_with_sublists[idx].append(my_string)
 
-    print(nested_list)
+    print(list_with_sublists)
 
 string1 = '.......'
 string2 = '*******'
@@ -54,6 +54,41 @@ def find_index_for_substr_end(input_string, newline_char, newline_occurence_need
                 return i
     return -1
 
+def get_three_line_substring(input_string):
+    newline_char = "\n"
+    starting_delimiter = 1
+    ending_delimiter = 4
+    total_newline_chars = input_string.count("\n")
+
+    while ending_delimiter <= total_newline_chars:
+
+        index_for_substr_start = find_index_for_substr_start(input_string, newline_char, starting_delimiter)
+
+        index_for_substr_end = find_index_for_substr_end(input_string, newline_char, ending_delimiter)
+
+        substring = input_string[index_for_substr_start:index_for_substr_end + 1]
+        substring_without_start_and_end_newlines = substring[1:-1]
+        # print(repr(substring_without_start_and_end_newlines))
+        
+
+        # break
+
+        starting_delimiter += 1
+        ending_delimiter += 1
+
+        return substring_without_start_and_end_newlines
+
+def split_three_line_substring(three_line_substring):
+    list_of_one_line_substrings = three_line_substring.split('\n')
+    return list_of_one_line_substrings
+
+def convert_strings_to_sublists(list_of_one_line_substrings):
+    list_with_sublists = []
+    for one_line_substring in list_of_one_line_substrings:
+        list_with_sublists.append(list(one_line_substring))
+
+    return list_with_sublists
+
 
 input_string = '''
 467..114..
@@ -67,24 +102,9 @@ input_string = '''
 ...$.*....
 .664.598..
 '''
-newline_char = "\n"
-starting_delimiter = 1
-ending_delimiter = 4
-total_newline_chars = input_string.count("\n")
+three_line_substring = get_three_line_substring(input_string)
+list_of_one_line_substrings = split_three_line_substring(three_line_substring)
+list_with_sublists = convert_strings_to_sublists(list_of_one_line_substrings)
+print(list_with_sublists)
 
-while ending_delimiter <= total_newline_chars:
 
-    index_for_substr_start = find_index_for_substr_start(input_string, newline_char, starting_delimiter)
-
-    index_for_substr_end = find_index_for_substr_end(input_string, newline_char, ending_delimiter)
-
-    # print(f'This is the index for substring start: {index_for_substr_start}')
-    # print(f'This is the index for substring end: {index_for_substr_end + 1}')
-
-    substring = input_string[index_for_substr_start:index_for_substr_end + 1]
-    print(repr(substring))
-
-    break
-
-    starting_delimiter += 1
-    ending_delimiter += 1
